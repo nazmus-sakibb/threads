09eb42aa1dd42ac133e61a306be88fac8a3eb3e8
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-10-22 15:59:27 +0600

    update package.json file

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,9 @@
 async function Page(){
-    return (
-        <main>
-            <h1 className="head-text">Onboarding</h1>
-        </main>
-    )
+  return (
+      <main>
+          <h1 className="head-text">Onboarding</h1>
+      </main>
+  )
 }
 
 
