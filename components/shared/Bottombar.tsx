806800ed0727c686b3fd000849dc5c714796dd66
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-10-24 00:18:47 +0600

    styling right sidebar layout

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -20,7 +20,7 @@ const Bottombar = () => {
             <Link
               href={link.route}
               key={link.label}
-              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
             >
               <Image
                 src={link.imgURL}
@@ -28,7 +28,7 @@ const Bottombar = () => {
                 width={24}
                 height={24}
               />
-              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">{link.label.split(/\s+./)[0]}</p>
             </Link>
           );
         })}
