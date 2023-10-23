2b84b4a35e77f50fcce7109e8e42c5749b9ae548
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-10-23 15:50:30 +0600

    style topbar layout

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
 
 const Topbar = () => {
     return (
         <nav className="topbar">
-            <h1>Topbar</h1>
+            <Link href='/' className="flex items-center gap-4">
+                <Image src="/logo.svg" alt="logo" width={28} height={28}/>
+                <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+            </Link>
         </nav>
     );
 };
