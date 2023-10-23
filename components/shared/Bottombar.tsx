9f3903707291beb62162d1ab533272b3f4c8cf06
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-10-23 23:22:32 +0600

    implement bottom navigation for mobile view

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,10 +1,40 @@
+"use client"
+
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Bottombar = () => {
-    return (
-        <div>
-            <h1>Bottombar</h1>
-        </div>
-    );
+    const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Bottombar;
