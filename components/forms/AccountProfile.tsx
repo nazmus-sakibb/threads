3df18c158942ef6f160fb1591d617c07862800db
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-10-24 12:58:13 +0600

    style onboarding form layout

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 "use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -10,11 +10,13 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
 import { userValidation } from "@/lib/validations/user";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { ChangeEvent } from "react";
+import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 interface Props {
@@ -30,48 +32,75 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
-
   const form = useForm({
     resolver: zodResolver(userValidation),
     defaultValues: {
-      profile_photo: '',
-      user: '',
-      username: '',
-      bio: '',
-    }
+      profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
   });
+
+
+  const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    e.preventDefault();
+  }
 
 
   function onSubmit(values: z.infer<typeof userValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
-
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col justify-start gap-10"
+      >
         <FormField
           control={form.control}
-          name="username"
+          name="profile_photo"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
+            <FormItem className="flex items-center gap-4">
+              <FormLabel className="account-form_image-label">
+                {field.value ? (
+                  <Image
+                    src={field.value}
+                    alt="profile photo"
+                    width={96}
+                    height={96}
+                    priority
+                    className="rounded-full object-contain"
+                  />
+                ) : (
+                  <Image
+                    src="/assets/profile.svg"
+                    alt="profile photo"
+                    width={24}
+                    height={24}
+                    className="object-contain"
+                  />
+                )}
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <Input 
+                  type="file"
+                  accept="image/*"
+                  placeholder="Upload a photo"
+                  className="account-form_image-input"
+                  onChange={(e) => handleImage(e, field.onChange)}
+                />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
             </FormItem>
           )}
         />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 };
 
 export default AccountProfile;
